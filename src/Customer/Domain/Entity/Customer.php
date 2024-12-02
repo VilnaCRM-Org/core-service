83c56b085000fc3d7f064171715fe435ba48185a
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-12-02 20:02:10 +0200

    feat(#4): add Customer entity

---
+++ src/Customer/Domain/Entity/Customer.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Customer\Domain\Entity;
+
+use DateTime;
+
+final class Customer
+{
+    public function __construct(
+        private ?string $id,
+        private ?string $initials,
+        private ?string $email,
+        private ?string $phone,
+        private ?string $leadSource,
+        private ?string $type,
+        private ?string $status,
+        private ?DateTime $dateCreated,
+        private ?DateTime $lastModifiedDate,
+        private bool $confirmed
+    ) {
+        $this->dateCreated = $dateCreated ?? new DateTime();
+        $this->lastModifiedDate = $lastModifiedDate ?? new DateTime();
+    }
+}
