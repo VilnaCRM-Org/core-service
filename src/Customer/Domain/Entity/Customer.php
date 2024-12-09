a8bb39dc8688bf147909e0ac7017000a2d3c152b
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-12-09 19:30:21 +0200

    fix(#4): fix issue with odm mapping

--- src/Customer/Domain/Entity/Customer.php
+++ src/Customer/Domain/Entity/Customer.php
@@ -9,15 +9,15 @@ use DateTime;
 final class Customer
 {
     public function __construct(
-        private ?string $id,
-        private ?string $initials,
-        private ?string $email,
-        private ?string $phone,
-        private ?string $leadSource,
-        private ?string $type,
-        private ?string $status,
-        private ?DateTime $dateCreated,
-        private ?DateTime $lastModifiedDate,
+        private string $id,
+        private string $initials,
+        private string $email,
+        private string $phone,
+        private string $leadSource,
+        private string $type,
+        private string $status,
+        private DateTime $dateCreated,
+        private DateTime $lastModifiedDate,
         private bool $confirmed
     ) {
         $this->dateCreated = $dateCreated ?? new DateTime();
