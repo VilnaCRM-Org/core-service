c595bddea578d61744aebe5b95d400e132bf3837
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-12-11 20:51:47 +0200

    feat(#4): add CommandResponseInterface and impl for create customer

---
+++ src/Customer/Application/Command/CreateCustomerCommandResponse.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Customer\Application\Command;
+
+use App\Customer\Domain\Entity\CustomerInterface;
+use App\Shared\Domain\Bus\Command\CommandResponseInterface;
+
+class CreateCustomerCommandResponse implements CommandResponseInterface
+{
+    public function __construct(public CustomerInterface $createdCustomer)
+    {
+    }
+}
