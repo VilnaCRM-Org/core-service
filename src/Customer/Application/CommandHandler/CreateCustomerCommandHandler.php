8d3ae2402d117e13f757c136084f44010a5ea962
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-12-12 21:30:11 +0200

    feat(#4): add CreateCustomerCommandHandler

---
+++ src/Customer/Application/CommandHandler/CreateCustomerCommandHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Customer\Application\CommandHandler;
+
+use App\Customer\Application\Command\CreateCustomerCommand;
+use App\Customer\Application\Command\CreateCustomerCommandResponse;
+use App\Customer\Application\Transformer\CreateCustomerTransformer;
+use App\Customer\Domain\Factory\Event\CustomerCreatedEventFactoryInterface;
+use App\Customer\Domain\Repository\CustomerRepositoryInterface;
+use App\Shared\Domain\Bus\Command\CommandHandlerInterface;
+use App\Shared\Domain\Bus\Event\EventBusInterface;
+use Symfony\Component\Uid\Factory\UuidFactory;
+
+class CreateCustomerCommandHandler implements CommandHandlerInterface
+{
+    public function __construct(
+        private CustomerRepositoryInterface $customerRepository,
+        private CreateCustomerTransformer $transformer,
+        private EventBusInterface $eventBus,
+        private UuidFactory $uuidFactory,
+        private CustomerCreatedEventFactoryInterface $customerCreatedEventFactory,
+    ) {
+    }
+
+    public function __invoke(CreateCustomerCommand $command): void
+    {
+        $user = $this->transformer->transformToCustomer($command);
+
+
+        $this->customerRepository->save($user);
+        $command->setResponse(new CreateCustomerCommandResponse($user));
+
+        $this->eventBus->publish(
+            $this->customerCreatedEventFactory->create(
+                $user,
+                (string) $this->uuidFactory->create()
+            )
+        );
+    }
+}
