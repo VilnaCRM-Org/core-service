527f23822e1701986f74201b3d6cf0822cd5d1d0
Kravalg <github-action@actions-template-sync.noreply.github.com>
2024-12-16 09:14:53 +0000

    chore(template): merge template changes :up:
    Signed-off-by: Kravalg <github-action@actions-template-sync.noreply.github.com>

--- tests/Unit/Internal/HealthCheck/Application/EventSub/DBCheckSubscriberTest.php
+++ tests/Unit/Internal/HealthCheck/Application/EventSub/DBCheckSubscriberTest.php
@@ -7,55 +7,28 @@ namespace App\Tests\Unit\Internal\HealthCheck\Application\EventSub;
 use App\Internal\HealthCheck\Application\EventSub\DBCheckSubscriber;
 use App\Internal\HealthCheck\Domain\Event\HealthCheckEvent;
 use App\Tests\Unit\UnitTestCase;
-use Doctrine\ODM\MongoDB\DocumentManager;
-use MongoDB\Client;
-use MongoDB\Driver\Exception\ConnectionException;
+use Doctrine\DBAL\Connection;
 
 final class DBCheckSubscriberTest extends UnitTestCase
 {
-    private DocumentManager $documentManager;
+    private Connection $connection;
     private DBCheckSubscriber $subscriber;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->documentManager = $this->createMock(DocumentManager::class);
-
-        $this->subscriber = new DBCheckSubscriber($this->documentManager);
+        $this->connection = $this->createMock(Connection::class);
+        $this->subscriber = new DBCheckSubscriber(
+            $this->connection
+        );
     }
 
-    public function testOnHealthCheckSuccess(): void
+    public function testOnHealthCheck(): void
     {
-        $clientMock = $this->createMock(Client::class);
-        $clientMock->expects($this->once())
-            ->method('listDatabases')
-            ->willReturn([]);
-
-        $this->documentManager->expects($this->once())
-            ->method('getClient')
-            ->willReturn($clientMock);
-
-        $event = new HealthCheckEvent();
-        $this->subscriber->onHealthCheck($event);
-    }
-
-    public function testOnHealthCheckFailure(): void
-    {
-        $clientMock = $this->createMock(Client::class);
-
-        $clientMock->expects($this->once())
-            ->method('listDatabases')
-            ->willThrowException(
-                new ConnectionException('Unable to connect to MongoDB')
-            );
-
-        $this->documentManager->expects($this->once())
-            ->method('getClient')
-            ->willReturn($clientMock);
-
-        $this->expectException(\RuntimeException::class);
-        $this->expectExceptionMessage('Unable to connect to MongoDB');
+        $this->connection->expects($this->once())
+            ->method('executeQuery')
+            ->with('SELECT 1');
 
         $event = new HealthCheckEvent();
         $this->subscriber->onHealthCheck($event);
