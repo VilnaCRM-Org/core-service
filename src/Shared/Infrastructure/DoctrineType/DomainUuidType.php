7f0f8d9dd8dfcd914f1226d1d2cf3d6506f58dcf
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-12-16 22:12:40 +0200

    feat(#4): refactor domain uud type

--- src/Shared/Infrastructure/DoctrineType/DomainUuidType.php
+++ src/Shared/Infrastructure/DoctrineType/DomainUuidType.php
@@ -7,7 +7,6 @@ namespace App\Shared\Infrastructure\DoctrineType;
 use App\Shared\Domain\ValueObject\Uuid;
 use App\Shared\Domain\ValueObject\UuidInterface;
 use Doctrine\ODM\MongoDB\Types\Type;
-use MongoDB\BSON\Binary;
 
 final class DomainUuidType extends Type
 {
@@ -16,29 +15,23 @@ final class DomainUuidType extends Type
     public function convertToDatabaseValue($value)
     {
         if ($value instanceof UuidInterface) {
-            return (string) $value;
+            return (string)$value;
         }
 
         if (is_string($value)) {
             return $value;
         }
-        if ($value instanceof Binary) {
-            $uuid = $value->getData();
-                return new Uuid($uuid);
-        }
+        $uuid = $value->getData();
+        return new Uuid($uuid);
     }
 
     public function convertToPHPValue($value)
     {
         if ($value instanceof UuidInterface) {
-            return $value; // Already converted.
+            return $value;
         }
 
-        if (is_string($value)) {
-            return new Uuid($value); // Convert string to Uuid object.
-        }
-
-        return null; // Handle null values gracefully.
+        return new Uuid($value);
     }
 
     public function closureToMongo(): string
