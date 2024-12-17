928c8292f3ff85d5273a59701bff27d0cb33f924
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-12-18 00:27:25 +0200

    feat(#4): add delete endpoint

---
+++ src/Customer/Application/CommandHandler/DeleteCustomerCommandHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Customer\Application\CommandHandler;
+
+use App\Customer\Domain\Repository\CustomerRepositoryInterface;
+use Composer\Repository\RepositoryInterface;
+
+class DeleteCustomerCommandHandler
+{
+public function __construct(CustomerRepositoryInterface $repository, )
+{
+}
+}
