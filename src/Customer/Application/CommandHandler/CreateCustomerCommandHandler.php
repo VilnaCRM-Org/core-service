29eab485fcc2b5799cd5c2b48ab1ad1b9f115e51
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-12-17 19:18:33 +0200

    feat(#4): add all customer fields into serialization file

--- src/Customer/Application/CommandHandler/CreateCustomerCommandHandler.php
+++ src/Customer/Application/CommandHandler/CreateCustomerCommandHandler.php
@@ -5,20 +5,15 @@ namespace App\Customer\Application\CommandHandler;
 use App\Customer\Application\Command\CreateCustomerCommand;
 use App\Customer\Application\Command\CreateCustomerCommandResponse;
 use App\Customer\Application\Transformer\CreateCustomerTransformer;
-use App\Customer\Domain\Factory\Event\CustomerCreatedEventFactoryInterface;
 use App\Customer\Domain\Repository\CustomerRepositoryInterface;
 use App\Shared\Domain\Bus\Command\CommandHandlerInterface;
-use App\Shared\Domain\Bus\Event\EventBusInterface;
 use Symfony\Component\Uid\Factory\UuidFactory;
 
 class CreateCustomerCommandHandler implements CommandHandlerInterface
 {
     public function __construct(
         private CustomerRepositoryInterface $customerRepository,
-        private CreateCustomerTransformer $transformer,
-        private EventBusInterface $eventBus,
-        private UuidFactory $uuidFactory,
-        private CustomerCreatedEventFactoryInterface $customerCreatedEventFactory,
+        private CreateCustomerTransformer $transformer
     ) {
     }
 
