7ef722f96ccb36988522172ef1e055b6c66965a8
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-12-24 23:05:38 +0200

    feat(#4): add CustomersReturnedResponseFactory

---
+++ src/Shared/Application/OpenApi/Factory/Response/CustomersReturnedResponseFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Application\OpenApi\Factory\Response;
+
+use App\Shared\Application\OpenApi\Builder\ArrayResponseBuilder;
+
+final class CustomersReturnedResponseFactory extends AbstractCustomerResponseFactory
+{
+    public function __construct(private ArrayResponseBuilder $responseBuilder)
+    {
+    }
+
+    protected function getResponseBuilder(): ArrayResponseBuilder
+    {
+        return $this->responseBuilder;
+    }
+
+    protected function getTitle(): string
+    {
+        return 'Customers created';
+    }
+}
