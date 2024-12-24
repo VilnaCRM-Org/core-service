90ce52e0e627b9ff1a6177cdb7304bc6b616944c
valerii <vsierhieiev-its24@lll.kpi.ua>
2024-12-24 23:00:34 +0200

    feat(#4): add getResponseBuilder method

--- src/Shared/Application/OpenApi/Factory/Response/CustomerCreatedResponseFactory.php
+++ src/Shared/Application/OpenApi/Factory/Response/CustomerCreatedResponseFactory.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace App\Shared\Application\OpenApi\Factory\Response;
 
+use App\Shared\Application\OpenApi\Builder\ResponseBuilder;
+
 final class CustomerCreatedResponseFactory extends AbstractCustomerResponseFactory
 {
+    public function __construct(private ResponseBuilder $responseBuilder)
+    {
+    }
+
+    protected function getResponseBuilder(): ResponseBuilder
+    {
+        return $this->responseBuilder;
+    }
+
     protected function getTitle(): string
     {
         return 'Customer created';
