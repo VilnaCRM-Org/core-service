0c83b9f222ef107de046de9e65a3c7a6b5336d83
valerii <vsierhieiev-its24@lll.kpi.ua>
2025-01-09 20:52:23 +0200

    refactor(#4): add requests factories

---
+++ src/Shared/Application/OpenApi/Factory/Request/AbstractCustomerRequestFactory.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Application\OpenApi\Factory\Request;
+
+use ApiPlatform\OpenApi\Model\RequestBody;
+use App\Shared\Application\OpenApi\Builder\Parameter;
+use App\Shared\Application\OpenApi\Builder\RequestBuilder;
+
+abstract class AbstractCustomerRequestFactory implements AbstractRequestFactoryInterface
+{
+    abstract protected function getRequestBuilder(): RequestBuilder;
+
+    public function getRequest(): RequestBody
+    {
+        return $this->getRequestBuilder()->build(
+            $this->getDefaultParameters()
+        );
+    }
+
+    protected function getDefaultParameters(): array
+    {
+        return [
+            $this->getEmailParam(),
+            $this->getPhoneParam(),
+            $this->getInitialsParam(),
+            $this->getLeadSourceParam(),
+            $this->getTypeParam(),
+            $this->getStatusParam(),
+            $this->getConfirmedParam(),
+        ];
+    }
+
+    protected function getConfirmedParam(): Parameter
+    {
+        return new Parameter('confirmed', 'boolean', true);
+    }
+
+    protected function getEmailParam(): Parameter
+    {
+        return new Parameter('email', 'string', 'customer@example.com', 255, 'email');
+    }
+
+    protected function getPhoneParam(): Parameter
+    {
+        return new Parameter('phone', 'string', '0123456789, 255');
+    }
+
+    protected function getLeadSourceParam(): Parameter
+    {
+        return new Parameter('leadSource', 'string', 'Google', 255);
+    }
+
+    protected function getTypeParam(): Parameter
+    {
+        return new Parameter(
+            'type',
+            'iri-reference',
+            '/api/customer_types/768e998b-31cb-419d-a02c-6ae9d5b4f447',
+            255
+        );
+    }
+
+    protected function getStatusParam(): Parameter
+    {
+        return new Parameter(
+            'status',
+            'iri-reference',
+            '/api/customer_statuses/c27f0884-8b6f-45db-858d-9a987a1d20d7',
+            255
+        );
+    }
+
+    protected function getInitialsParam(): Parameter
+    {
+        return new Parameter('initials', 'string', 'Name Surname', 255);
+    }
+}
---
+++ src/Shared/Application/OpenApi/Factory/Request/CustomerRequestFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Application\OpenApi\Factory\Request;
+
+use App\Shared\Application\OpenApi\Builder\RequestBuilder;
+
+final class CustomerRequestFactory extends AbstractCustomerRequestFactory
+{
+    public function __construct(private RequestBuilder $requestBuilder)
+    {
+    }
+
+    protected function getRequestBuilder(): RequestBuilder
+    {
+        return $this->requestBuilder;
+    }
+}
