039c137a9273fd69bbfb61dde6a7078b562468a2
valerii <vsierhieiev-its24@lll.kpi.ua>
2025-01-09 20:43:17 +0200

    refactor(#4): add factory for PATCH response

---
+++ src/Shared/Application/OpenApi/Factory/Response/CustomerUpdatedResponseFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Application\OpenApi\Factory\Response;
+
+use App\Shared\Application\OpenApi\Builder\ResponseBuilder;
+
+class CustomerUpdatedResponseFactory extends AbstractCustomerResponseFactory
+{
+    public function __construct(private ResponseBuilder $responseBuilder)
+    {
+    }
+
+    protected function getResponseBuilder(): ResponseBuilder
+    {
+        return $this->responseBuilder;
+    }
+
+    protected function getTitle(): string
+    {
+        return 'User updated';
+    }
+}
