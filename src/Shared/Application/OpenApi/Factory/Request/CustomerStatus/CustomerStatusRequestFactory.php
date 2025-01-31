6fda84891abc97191fcc2bbae536fffe929eb0b7
valerii <vsierhieiev-its24@lll.kpi.ua>
2025-01-31 19:48:17 +0200

    refactor(#4): add open api spec for customer type and status

---
+++ src/Shared/Application/OpenApi/Factory/Request/CustomerStatus/CustomerStatusRequestFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Application\OpenApi\Factory\Request\CustomerStatus;
+
+use App\Shared\Application\OpenApi\Builder\RequestBuilder;
+
+final class CustomerStatusRequestFactory extends AbstractCustomerStatusRequestFactory
+{
+    public function __construct(private RequestBuilder $requestBuilder)
+    {
+    }
+
+    protected function getRequestBuilder(): RequestBuilder
+    {
+        return $this->requestBuilder;
+    }
+}
