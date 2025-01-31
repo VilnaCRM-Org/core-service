20ff9d7781841c7a515a4f57b1d4779a70e1a8d8
valerii <vsierhieiev-its24@lll.kpi.ua>
2025-01-31 19:48:17 +0200

    refactor(#4): add open api spec for customer type and status

---
+++ src/Shared/Application/OpenApi/Factory/Response/CustomerType/CustomerTypeDeletedResponseFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Application\OpenApi\Factory\Response\CustomerType;
+
+use ApiPlatform\OpenApi\Model\Response;
+use App\Shared\Application\OpenApi\Builder\ResponseBuilder;
+use App\Shared\Application\OpenApi\Factory\Response\ResponseFactoryInterface;
+
+final class CustomerTypeDeletedResponseFactory implements ResponseFactoryInterface
+{
+    public function __construct(private ResponseBuilder $responseBuilder)
+    {
+    }
+
+    public function getResponse(): Response
+    {
+        return $this->responseBuilder->build(
+            'CustomerType resource deleted',
+            [],
+            []
+        );
+    }
+}
