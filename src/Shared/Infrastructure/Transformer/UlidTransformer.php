f0cec7c19fbdd43049a62b31bdd6277cfdee8176
valerii <vsierhieiev-its24@lll.kpi.ua>
2025-02-12 18:52:33 +0200

    refactor(#15): fix error with binary ULID storage

--- src/Shared/Infrastructure/Transformer/UlidTransformer.php
+++ src/Shared/Infrastructure/Transformer/UlidTransformer.php
@@ -5,17 +5,19 @@ declare(strict_types=1);
 namespace App\Shared\Infrastructure\Transformer;
 
 use MongoDB\BSON\Binary;
-use Symfony\Component\Uid\AbstractUid;
 use Symfony\Component\Uid\Ulid;
 
 final readonly class UlidTransformer
 {
-    public function transformFromString(string $value): ?Ulid
+    public function transformFromString($value): ?Ulid
     {
+        if ($value instanceof Binary) {
+            return Ulid::fromRfc4122($value->getData());
+        }
         return $value ? Ulid::fromString($value) : null;
     }
 
-    public function transformFromSymfonyUlid(AbstractUid $value): ?Binary
+    public function transformFromSymfonyUlid($value): ?Binary
     {
         return $value instanceof Ulid
             ? new Binary($value->toBinary(), Binary::TYPE_GENERIC)
