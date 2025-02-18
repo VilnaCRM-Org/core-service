4babcc4a1e3cb2509110ca626b33e40e8e5c1d1e
valerii <vsierhieiev-its24@lll.kpi.ua>
2025-02-18 15:21:07 +0200

    refactor(#15): add for patch request specific headers

--- src/Shared/Application/OpenApi/Factory/Request/CustomerStatus/UpdateCustomerStatusRequestFactory.php
+++ src/Shared/Application/OpenApi/Factory/Request/CustomerStatus/UpdateCustomerStatusRequestFactory.php
@@ -4,15 +4,16 @@ declare(strict_types=1);
 
 namespace App\Shared\Application\OpenApi\Factory\Request\CustomerStatus;
 
-use App\Shared\Application\OpenApi\Builder\RequestBuilder;
+use App\Shared\Application\OpenApi\Builder\RequestBuilderInterface;
+use App\Shared\Application\OpenApi\Builder\RequestPatchBuilder;
 
 class UpdateCustomerStatusRequestFactory extends AbstractCustomerStatusRequestFactory
 {
-    public function __construct(private RequestBuilder $requestBuilder)
+    public function __construct(private RequestPatchBuilder $requestBuilder)
     {
     }
 
-    protected function getRequestBuilder(): RequestBuilder
+    protected function getRequestBuilder(): RequestBuilderInterface
     {
         return $this->requestBuilder;
     }
