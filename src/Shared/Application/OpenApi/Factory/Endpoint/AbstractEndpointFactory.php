5ff7b4602772554ac52db6fd1f7d23c952b3a78b
valerii <vsierhieiev-its24@lll.kpi.ua>
2025-02-21 18:23:47 +0200

    refactor(#15): refactor formatting according to phpinsights

--- src/Shared/Application/OpenApi/Factory/Endpoint/AbstractEndpointFactory.php
+++ src/Shared/Application/OpenApi/Factory/Endpoint/AbstractEndpointFactory.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace App\Shared\Application\OpenApi\Factory\Endpoint;
 
+use ApiPlatform\OpenApi\Model\Response;
+
 abstract class AbstractEndpointFactory implements AbstractEndpointFactoryInterface
 {
+    /**
+     * @param array<int,Response> $baseResponses
+     * @param array<int,Response> $overrideResponses
+     * @return array<int,Response>
+     */
     public function mergeResponses(
         array $baseResponses,
         array $overrideResponses,
