89a5d7d71f69f66451238210fd3e4fa8e54882f0
valerii <vsierhieiev-its24@lll.kpi.ua>
2025-02-28 15:51:34 +0200

    refactor(#4): change uuid to ulid in abstract factory class

--- src/Shared/Application/OpenApi/Factory/UriParameter/CustomerFactory.php
+++ src/Shared/Application/OpenApi/Factory/UriParameter/CustomerFactory.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Shared\Application\OpenApi\Factory\UriParameter;
 
-final class CustomerFactory extends UuidUriParameterFactory
+final class CustomerFactory extends UlidUriParameterFactory
 {
     protected function getDescription(): string
     {
--- src/Shared/Application/OpenApi/Factory/UriParameter/CustomerTypeFactory.php
+++ src/Shared/Application/OpenApi/Factory/UriParameter/CustomerTypeFactory.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Shared\Application\OpenApi\Factory\UriParameter;
 
-final class CustomerTypeFactory extends UuidUriParameterFactory
+final class CustomerTypeFactory extends UlidUriParameterFactory
 {
     protected function getDescription(): string
     {
