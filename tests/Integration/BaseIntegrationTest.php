9d4d67bd2a7004bd79d4fb0f0eb6ecdef11192cf
valerii <vsierhieiev-its24@lll.kpi.ua>
2025-03-27 20:18:18 +0200

    feat(#26): add integration tests coverage

---
+++ tests/Integration/BaseIntegrationTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Tests\Unit\UlidProvider;
+use Faker\Factory;
+use Faker\Generator;
+
+abstract class BaseIntegrationTest extends ApiTestCase
+{
+    protected Generator $faker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Factory::create();
+        $this->faker->addProvider(new UlidProvider($this->faker));
+    }
+
+    /**
+     * @param array  $payload Request body to be JSON encoded (if any)
+     * @param array  $headers Optional additional headers
+     *
+     * @return array The response data as an array
+     */
+    protected function jsonRequest(
+        string $method,
+        string $uri,
+        array $payload = [],
+        array $headers = []
+    ): array {
+        $client = self::createClient();
+
+        $defaultHeaders = [
+            'Content-Type' => 'application/ld+json',
+        ];
+        $headers = array_merge($defaultHeaders, $headers);
+        $body = empty($payload) ? null : json_encode($payload);
+
+        $response = $client->request(
+            $method,
+            $uri,
+            [
+                'headers' => $headers,
+                'body' => $body,
+            ]
+        );
+        return $response->toArray();
+    }
+
+    protected function assertCreatedResponse(array $payload, array $responseData): void
+    {
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertResponseHeaderSame(
+            'content-type',
+            'application/ld+json; charset=utf-8'
+        );
+        $this->assertArrayHasKey('@id', $responseData);
+        if (isset($payload['email'])) {
+            $this->assertSame($payload['email'], $responseData['email']);
+        }
+    }
+
+    /**
+     * @param array $payload
+     */
+    protected function createEntity(string $uri, array $payload, ?string $expectedType = null): string
+    {
+        $data = $this->jsonRequest('POST', $uri, $payload);
+        $this->assertResponseStatusCodeSame(201);
+        if ($expectedType !== null) {
+            $this->assertSame($expectedType, (string) $data['@type']);
+        }
+        return (string) $data['@id'];
+    }
+}
